d84c9b0c0b74d1535664e9012b65e6ea2bb141fc
Petar Scepanovic <hiborija@gmail.com>
2020-03-23 21:00:47 +0100

    still work in progress

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,17 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
-
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.reg = [0] * 8
+        self.ram = [0] * 256
+        self.pc = 0
+        self.mar = 0
+        self.mdr = 0
+        self.running = True
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +23,39 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
+            0b10000010,  # LDI R0,8
+            0b00000000,  #RO
+            0b00001000,  # 8
+            0b01000111,  # PRN R0
+            0b00000000,  # RO
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
+    def ram_read(self, index):
+        self.mar = index
+        self.mdr = self.ram[self.mar]
+        return self.mdr
 
-    def alu(self, op, reg_a, reg_b):
+    def ram_write(self, index, value):
+        self.mar = index
+        self.mdr = value
+        self.ram[self.mar] = self.mdr
+
+    def alu(self, op, reg_a=None, reg_b=None):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == 'LDI':
+            self.reg[reg_a] = reg_b
+        elif op == 'PRN':
+            print(self.reg[reg_a])
+        elif op == 'HLT':
+            self.running = False
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -46,14 +65,10 @@ class CPU:
         from run() if you need help debugging.
         """
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            #self.fl,
-            #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
-        ), end='')
+        print(f"TRACE: %02X | %02X %02X %02X |" %
+              (self.pc, self.ram_read(self.pc), self.ram_read(self.pc + 1),
+               self.ram_read(self.pc + 2)),
+              end='')
 
         for i in range(8):
             print(" %02X" % self.reg[i], end='')
@@ -62,4 +77,25 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+
+        while self.running:
+            command = self.ram[self.pc]
+
+            command_string = format(command, '#010b')
+
+            instruction_bits = command_string[2:4]
+
+            if instruction_bits == '10':
+                operand_a = self.ram_read(self.pc + 1)
+                operand_b = self.ram_read(self.pc + 2)
+            elif instruction_bits == '01':
+                operand_a = self.ram_read(self.pc + 1)
+                operand_b = None
+            else:
+                operand_a = None
+                operand_b = None
+
+   
+
+convert = int('0b00001000', 2)
+print(convert)
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -6,6 +6,9 @@ import sys
 from cpu import *
 
 cpu = CPU()
+# if len(sys.argv) != True:
+#     print("there are no files")
+#     sys.exit()
 
 cpu.load()
 cpu.run()
